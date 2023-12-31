089bfb22a33bccd8305bf59ca6df1bb2192c8ac6
Eric Bartusch <ericbartusch8@gmail.com>
2023-12-30 22:06:34 -0600

    Add first draft of conduit

--- src/main/java/spireMapOverhaul/zones/storm/StormZone.java
+++ src/main/java/spireMapOverhaul/zones/storm/StormZone.java
@@ -1,35 +1,22 @@
 package spireMapOverhaul.zones.storm;
 
-import basemod.BaseMod;
 import basemod.helpers.CardModifierManager;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.monsters.MonsterGroup;
-import com.megacrit.cardcrawl.random.Random;
-import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.rewards.RewardItem;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
-import com.megacrit.cardcrawl.rooms.MonsterRoomElite;
-import spireMapOverhaul.SpireAnniversary6Mod;
 import spireMapOverhaul.abstracts.AbstractZone;
 import spireMapOverhaul.zoneInterfaces.CombatModifyingZone;
-import spireMapOverhaul.zoneInterfaces.EncounterModifyingZone;
 import spireMapOverhaul.zoneInterfaces.RewardModifyingZone;
-import spireMapOverhaul.zoneInterfaces.ShopModifyingZone;
-import spireMapOverhaul.zones.invasion.InvasionUtil;
-import spireMapOverhaul.zones.invasion.cards.HandOfTheAbyss;
-import spireMapOverhaul.zones.invasion.monsters.*;
 import spireMapOverhaul.zones.storm.cardmods.DampModifier;
+import spireMapOverhaul.zones.storm.patches.AddLightningPatch;
+import spireMapOverhaul.zones.storm.powers.ConduitPower;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
-import static spireMapOverhaul.util.Wiz.atb;
+import static spireMapOverhaul.util.Wiz.*;
 import static spireMapOverhaul.zones.storm.StormUtil.cardValidToMakeDamp;
 import static spireMapOverhaul.zones.storm.StormUtil.countValidCardsInHandToMakeDamp;
 
@@ -82,5 +69,18 @@ public class StormZone extends AbstractZone implements CombatModifyingZone, Rewa
                 }
             });
         }
+
+        ArrayList<AbstractMonster> mons = getEnemies();
+        int totalActors = mons.size() + 1;
+
+        if (AbstractDungeon.cardRandomRng.random(1, totalActors) == 1) {
+            AddLightningPatch.AbstractRoomFields.conduitTarget.set(AbstractDungeon.getCurrRoom(), AbstractDungeon.player);
+            applyToSelf(new ConduitPower(AbstractDungeon.player));
+        } else {
+            AbstractMonster m = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
+            AddLightningPatch.AbstractRoomFields.conduitTarget.set(AbstractDungeon.getCurrRoom(), m);
+            applyToEnemy(m, new ConduitPower(m));
+        }
+
     }
 }
--- src/main/java/spireMapOverhaul/zones/storm/patches/AddLightningPatch.java
+++ src/main/java/spireMapOverhaul/zones/storm/patches/AddLightningPatch.java
@@ -6,11 +6,13 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireField;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import spireMapOverhaul.zones.storm.StormUtil;
 import spireMapOverhaul.zones.storm.vfx.LightningEffect;
+import spireMapOverhaul.zones.storm.vfx.LightningOrbPassiveEffect;
 
 import static spireMapOverhaul.util.Wiz.atb;
 
@@ -19,25 +21,50 @@ public class AddLightningPatch {
 
     @SpirePatch(clz = AbstractRoom.class, method = SpirePatch.CLASS)
     public static class AbstractRoomFields {
-        public static SpireField<Float> timeToStrike = new SpireField(() -> 4.0f);
-        public static SpireField<Float> timeSinceStrike = new SpireField(() -> 1.0f);
+        public static SpireField<Float> timeToStrike = new SpireField<>(() -> 4.0f);
+        public static SpireField<Float> timeSinceStrike = new SpireField<>(() -> 1.0f);
+        public static SpireField<AbstractCreature> conduitTarget = new SpireField<>(() -> null);
     }
 
     @SpirePatch(clz = AbstractRoom.class, method = "update")
-    public static class Strike {
+    public static class GreaseLightning {
 
+        private static float vfxTimer;
+        private static float scale = 0.5F;
+        private static float timeScaleStart = 0.1F;
+        private static float timeScaleEnd = 0.3F;
         @SpirePrefixPatch()
         public static void Prefix() {
             if(StormUtil.isInStormZone()) {
                 if (AbstractRoomFields.timeToStrike.get(AbstractDungeon.getCurrRoom()) < 0.0f) {
-                    float rand_y = MathUtils.random((Settings.HEIGHT / 2) - 50.0f * Settings.scale, (Settings.HEIGHT / 2) - 350.0f * Settings.scale);
-                    boolean renderBehind = rand_y > Settings.HEIGHT / 2 - 250.0f;
+                    float rand_y = MathUtils.random(((float) Settings.HEIGHT / 2) - 50.0f * Settings.scale, ((float) Settings.HEIGHT / 2) - 350.0f * Settings.scale);
+                    boolean renderBehind = rand_y > (float) Settings.HEIGHT / 2 - 250.0f;
                     atb(new VFXAction(new LightningEffect(MathUtils.random(Settings.WIDTH), rand_y, renderBehind)));
                     AbstractRoomFields.timeToStrike.set(AbstractDungeon.getCurrRoom(), MathUtils.random(3.5f, 10.0f));
                     AbstractRoomFields.timeSinceStrike.set(AbstractDungeon.getCurrRoom(), 0.0f);
                 }
                 AbstractRoomFields.timeToStrike.set(AbstractDungeon.getCurrRoom(), AbstractRoomFields.timeToStrike.get(AbstractDungeon.getCurrRoom()) - Gdx.graphics.getDeltaTime());
                 AbstractRoomFields.timeSinceStrike.set(AbstractDungeon.getCurrRoom(), AbstractRoomFields.timeSinceStrike.get(AbstractDungeon.getCurrRoom()) + Gdx.graphics.getDeltaTime());
+
+                AbstractCreature conduitTarget = AbstractRoomFields.conduitTarget.get(AbstractDungeon.getCurrRoom());
+                if(conduitTarget != null) {
+                    vfxTimer -= Gdx.graphics.getDeltaTime();
+                    if (vfxTimer < 0.0F) {
+                        AbstractDungeon.effectList.add(new LightningOrbPassiveEffect(MathUtils.random(conduitTarget.hb.cX + conduitTarget.hb.width * scale, conduitTarget.hb.cX - conduitTarget.hb.width * scale), MathUtils.random(conduitTarget.hb.cY + conduitTarget.hb.height * scale, conduitTarget.hb.cY - conduitTarget.hb.height * scale)));
+                        if (MathUtils.randomBoolean()) {
+                            AbstractDungeon.effectList.add(new LightningOrbPassiveEffect(MathUtils.random(conduitTarget.hb.cX + conduitTarget.hb.width * scale, conduitTarget.hb.cX - conduitTarget.hb.width * scale), MathUtils.random(conduitTarget.hb.cY + conduitTarget.hb.height * scale, conduitTarget.hb.cY - conduitTarget.hb.height * scale)));
+                        }
+                        vfxTimer = MathUtils.random(timeScaleStart, timeScaleEnd);
+                        scale *= 0.5F;
+                        timeScaleStart -= 0.01F;
+                        timeScaleEnd -= 0.03F;
+                        if(!AbstractDungeon.actionManager.turnHasEnded) { //close into center if turn ends
+                            scale = 0.5f;
+                            timeScaleStart = 0.1f;
+                            timeScaleEnd = 0.3F;
+                        }
+                    }
+                }
             }
         }
     }
